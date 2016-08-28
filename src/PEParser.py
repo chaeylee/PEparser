a48c2177f21b57acbaf12899e618e82642dbf5d6
kyungmin2 <km9311@naver.com>
2016-08-28 13:50:50 +0900

    Update PEParser.py
    도스헤더까지 만듬

--- src/PEParser.py
+++ src/PEParser.py
@@ -6,25 +6,65 @@ where <filename> is a PE file.
 
 import sys
 import struct
+import binascii
+from struct import unpack
 
-class Usage(Exception):
-    def __init__(self, msg):
-        self.msg = msg
 
-def main():
 
+
+#print(binascii.hexlify(pe.DOS_HEADER['e_magic']))
+
+
+class PeHeader:
+    
+    def __init__(self,b_data):
+        self.dos_header = IMAGE_DOS_HEADER(b_data)  # dos_header는 IMAGE_DOS_HEADER 클래스의 인스턴스이다.
+        
+        
+        
+
+
+
+class IMAGE_DOS_HEADER:
+
+    DOS_HEADER = {}  # 도스헤더의 필드를 담는 딕셔너리
+
+    def __init__(self,b_data):
+        
+        self.DOS_HEADER['e_magic'] = binascii.hexlify(b_data[0:2])
+        self.DOS_HEADER['e_lfanew'] = binascii.hexlify(b_data[60:64])
+
+    def GetE_magic(self):
+        return self.DOS_HEADER['e_magic']
+
+    def GetE_lfanew(self):
+        return self.DOS_HEADER['e_lfanew']
+
+
+
+        
+if __name__ == '__main__':
+
+   
+    
     if (len(sys.argv) < 2):
-        raise Usage("File name is required. Please check the usage.")
+        print("File name is required. Please check the usage.")
+        exit(1)
+
+
 
     filename = sys.argv[1]
-    f = open(filename, 'r+b')
-    try:
-        byte = f.read(1)
-        while byte != None:
-            print('{0:08b}'.format(ord(byte)))
-            byte = f.read(1)
-    finally:
-        f.close()
+    f = open(filename, 'rb')
+    b_data = f.read()       # b_data는 프로그램의 바이너리값이 들어있다.
 
-if __name__ == '__main__':
-    sys.exit(main())
+    pe = PeHeader(b_data)   # pe는 PeHeader 클래스의 인스턴스이다
+ 
+    print(pe.dos_header.GetE_magic())  # 도스헤더의 e_magic 필드 출력
+    print(pe.dos_header.GetE_lfanew()) # 도스헤더의 e_lfanew 필드 출력
+    
+
+    f.close()
+    
+
+    
+    

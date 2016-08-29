1eb15c8b6d575a11fe5d47a953793bdf5eb0f8dd
Iron <helloiron@Irons-MBP.home>
2016-08-28 21:02:30 -0400

    Update src

--- src/PEParser.py
+++ src/PEParser.py
@@ -1,12 +1,27 @@
 """ PE Parser
 A simple program for parsing PE header
 Usage: python3 PEParser.py <filename>
-where <filename> is a PE file.
+file should be a PE file.
 """
 
 import sys
 from PEHeader import *
 
+def main():
+    if (len(sys.argv) < 2):
+        print("File name is required. Please check the usage.")
+        exit(1)
+
+    filename = sys.argv[1]
+    if validateFileType(filename) is False:
+        print("File type is not acceptable. Please load PE File only.")
+        exit(1)
+    f = open(filename, 'rb')
+    b_data = f.read()       # b_data는 프로그램의 바이너리값이 들어있다.
+
+    pe = PEHeader(b_data)   # pe는 PeHeader 클래스의 인스턴스이다
+    print(pe)
+    f.close()
 
 def validateFileType(filename):
     if filename.endswith('.exe') is True:
@@ -40,23 +55,6 @@ def loadFile(filename):
         pe = PEHeader(b_data)   # pe는 PeHeader 클래스의 인스턴스이다
     f.close()
     return pe
-
-
-def main():
-    if (len(sys.argv) < 2):
-        print("File name is required. Please check the usage.")
-        exit(1)
-
-    filename = sys.argv[1]
-    if validateFileType(filename) is False:
-        print("File type is not acceptable. Please load PE File only.")
-        exit(1)
-    f = open(filename, 'rb')
-    b_data = f.read()       # b_data는 프로그램의 바이너리값이 들어있다.
-
-    pe = PEHeader(b_data)   # pe는 PeHeader 클래스의 인스턴스이다
-    print(pe)
-    f.close()
 
 if __name__ == '__main__':
     main()
